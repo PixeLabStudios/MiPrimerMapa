21d0fbce9a18ec98e28f583c50a1f453d6f4b152
RafaelFascio <rafaelfascio@gmail.com>
2025-06-18 21:53:13 -0300

    Woman Antartic
    Agrego a la mujer en la antartida sin animaciones

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class Singleton : MonoBehaviour
 {
     public static Singleton Instance;
+    #region M
     public int hairIndex = 0;
     public int colorIndex = 0;
     public int skinColorIndex = 0;
-    public int accessoryIndex = 0;
+    //public int accessoryIndex = 0;
     public int chestIndex = 0;
     public int legsIndex = 0;
     public int feetIndex = 0;
@@ -15,6 +16,24 @@ public class Singleton : MonoBehaviour
     public int colorIndexChest = 0;
     public int colorIndexLegs = 0;
     public int colorIndexFeet = 0;
+    #endregion
+
+    #region F
+    public int hairIndexF = 0;
+    public int colorIndexF = 0;
+    public int skinColorIndexF = 0;
+    //public int accessoryIndexF = 0;
+    public int chestIndexF = 0;
+    public int legsIndexF = 0;
+    public int feetIndexF = 0;
+
+    public int colorIndexHairF = 0;
+    public int colorIndexChestF = 0;
+    public int colorIndexLegsF = 0;
+    public int colorIndexFeetF = 0;
+    #endregion
+
+    public bool isMan;
 
     void Awake()
     {
