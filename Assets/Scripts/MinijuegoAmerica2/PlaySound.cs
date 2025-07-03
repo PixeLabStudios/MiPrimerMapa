302fdbbed1ca2a05d911431a7c233a624ff6642e
AdrianSardina <112350190+AdrianSardina@users.noreply.github.com>
2025-07-03 16:28:40 -0300

    arreglando bug

--- Assets/Scripts/MinijuegoAmerica2/PlaySound.cs
+++ Assets/Scripts/MinijuegoAmerica2/PlaySound.cs
@@ -2,24 +2,35 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
-   public AudioClip clip;
+   America2Manager manager;
+    public AudioClip clip;
+   
 
-    bool isPlaying = false;
+    private void Awake()
+    {
+        manager = FindFirstObjectByType<America2Manager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !isPlaying)
+        if (other.CompareTag("Player") )
         {
-            isPlaying = true;
-            AudioSource.PlayClipAtPoint(clip, transform.position);
+           manager.audioSource.resource = clip;
+            
+           manager.audioSource.Play();
+            // Reproduce el sonido 
+            manager.audioSource.PlayOneShot(clip);
+           
+            // Aquí puedes agregar lógica adicional si es necesario
+            // Por ejemplo, actualizar el estado del juego o la UI
+            
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlaying = false;
-            clip.
+            manager.audioSource.Stop();
         }
     }
 }
