8f534208bda2ea5d5fef230695fb3914b0b5153b
RafaelFascio <rafaelfascio@gmail.com>
2026-09-10 00:56:10 -0300

    effects

--- Assets/Scripts/MinijuegoEuropa2/TurretScript.cs
+++ Assets/Scripts/MinijuegoEuropa2/TurretScript.cs
@@ -25,9 +25,10 @@ public class TurretScript : MonoBehaviour
     {
         while (true) 
         {
-            GameObject b = Instantiate(prefab,spawnLocation.position,prefab.transform.rotation);
+            GameObject b = Instantiate(prefab,spawnLocation.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y + 270, transform.rotation.z));
+            b.transform.LookAt(manager.drakkarScript.GetDrakkarPos());
             Bulletship script = b.GetComponent<Bulletship>();
-            script.transform.localScale *= scale;
+            //script.transform.localScale *= scale;
             script.speed = speed;
             b.GetComponent<Bulletship>().SetTarget(manager.drakkarScript.GetDrakkarPos(),spawnLocation.position) ;
             yield return new WaitForSeconds(fireCooldown);
@@ -39,11 +40,25 @@ public class TurretScript : MonoBehaviour
         direction = Vector3.Normalize(direction);
         float angleX = Mathf.Atan2(direction.z, direction.y) * Mathf.Rad2Deg;
         float angleY = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.Euler(0,angleY+180,0);
+        Quaternion rotation = Quaternion.Euler(0,angleY+90,0);
         rotation = Quaternion.Normalize(rotation);
        transform.rotation = Quaternion.RotateTowards(transform.rotation,rotation,90*Time.deltaTime);
         
     }
+    void LookAtPlayer2()
+    {
+        Vector3 direction = manager.drakkarScript.GetDrakkarPos() - spawnLocation.position;
+
+        // Ignoramos la inclinación en Y para que la torreta solo gire horizontalmente
+        direction.y = 0;
+
+        if (direction != Vector3.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 90 * Time.deltaTime);
+        }
+    }
+
     public void OnDisable()
     {
         StopCoroutine(SingleShot());
